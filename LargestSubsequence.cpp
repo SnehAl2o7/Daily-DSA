435eb449d4002b208c238846e62a1f44bde79181
SnehAl207 <snehalsingh353@gmail.com>
2025-04-23 12:01:07 +0530

    updated with optimum

--- LargestSubsequence.cpp
+++ LargestSubsequence.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Solution {
     public:
+    // own came up approach
         int longestConsecutive(vector<int>& nums) {
             int n = nums.size();
             if(n ==0)return 0;
@@ -23,11 +24,31 @@ class Solution {
             }
             return Max_cnt;
         }
+
+    // optimized approach or required one
+
+    int longestConsecutiveOpt(vector<int> & nums){
+        int longest = 0;
+
+        unordered_set<int> seq(nums.begin(),nums.end());
+        
+        for(auto it : seq){
+            if(seq.find(it-1) == seq.end()){
+                int cnt = 1;
+                while(seq.find(it+cnt) != seq.end()){
+                    cnt += 1;
+                }
+                longest = max(cnt,longest);
+            }
+        }
+        return longest;
+    }
     };
 
 int main(){
     Solution obj;
     vector<int> nums = {100, 4, 200, 1, 3, 2};
     cout << obj.longestConsecutive(nums) << endl;
+    cout << obj.longestConsecutiveOpt(nums) << endl;
     return 0;
 }
