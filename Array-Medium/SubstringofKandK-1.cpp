05807a4a26742afa5e719ff792abdd30ceb595f7
SnehAl2o7 <snehalsingh353@gmail.com>
2025-07-01 23:44:16 +0530

    solving the substring medium level question

---
+++ Array-Medium/SubstringofKandK-1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solutions{
+    public:
+    int LongestValue(string s,int k){
+        unordered_map<char, int> m1;
+        int distinct = 0;
+        int ans = 0;
+        for(int i = 0; i < k; i++){
+            m1[s[i]]++;
+            if(m1[s[i]] == 1){
+                distinct++;
+            }
+        }
+        if(distinct == k-1){
+            ans++;
+        }
+        int i = 0;
+        int j = k;
+        while(j < s.size()){
+            m1[s[j]]++;
+            if(m1[s[j]] == 1){
+                distinct++;
+            }
+            m1[s[i]]--;
+            if(m1[s[i]] == 0){
+                distinct--;
+            }
+            if(distinct == k-1){
+                ans++;
+            }
+            i++;
+            j++;
+        }
+        return ans;
+    }
+};
+
+int main(){
+    
+}
