ec87a00dd117e6c6ab7ef87b7fc905df2999deb3
SnehAl2o7 <snehalsingh353@gmail.com>
2025-07-19 23:44:18 +0530

    solving DSA hard problems

---
+++ StringOperations/NumberofWordsFromVowel.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int vowelCount(string& s) {
+        map<char, int> mp;
+        
+        for(char c: s)
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') mp[c]++;
+            
+        int sz = mp.size(), ans = sz;
+        
+        
+        for(int i = 2; i<sz; i++)  ans *= i;
+        for(auto it: mp)    ans *= it.second;
+        
+        return ans;
+    }
+};
+
+int main(){
+    
+}
+
