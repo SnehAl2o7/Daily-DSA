5dcfe9a26bac5ce2b69392450730a1bc7ce1bc22
SnehAl2o7 <165368874+SnehAl2o7@users.noreply.github.com>
2025-08-13 23:44:00 +0530

    Create Code.cpp

---
+++ Code.cpp
@@ -0,0 +1,16 @@
+int n=arr.size();
+        int safe_troop=(n+1)/2;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=arr[i]%k!=0?k-arr[i]%k:0;
+        }
+        sort(arr.begin(),arr.end());
+        int i=0;
+        int ans=0;
+        while(safe_troop)
+        {
+            ans+=arr[i];
+            i++;
+            safe_troop--;
+        }
+        return ans;
